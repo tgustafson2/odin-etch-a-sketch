d3037fa70959378861bc7ed1c37d578eb283084d
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-25 12:06:17 -0800

    Finish etch-a-sketch project and extra challenges
     - Implemented a button that allows users to change number of boxes per
       grid side
    
     - Made it so a background color is randomly assigned upon first mouse
       over by users
    
     - Made it so initial opacity is .1 and incremented by .1 upon subsequent
       mouseover without changing initial color

--- source.js
+++ source.js
@@ -6,10 +6,10 @@ for(let i = 0; i<16; i++){
     row.className = "grid-row";
     for(let j = 0; j<16; j++){
         const box = document.createElement("div");
-        box.addEventListener("mouseover", (elem) =>{
-            console.log(elem.target);
-            elem.target.style.backgroundColor = "blue";
-            console.log(elem.target.style.backgroundColor);
+        box.addEventListener("mouseover", (e) =>{
+
+            setColor(e.target);
+
         })
         box.className = "grid-box";
         row.appendChild(box);
@@ -17,5 +17,60 @@ for(let i = 0; i<16; i++){
     grid.appendChild(row);
 }
 
+function setColor(elem){
+    console.log(elem.style.backgroundColor);
+    if(elem.style.backgroundColor == ""){
+        const rValue = Math.round(Math.random()*255);
+        const gValue = Math.round(Math.random()*255);
+        const bValue = Math.round(Math.random()*255);
+        elem.style.backgroundColor = "rgb(" + rValue + "," + gValue + "," + bValue + ")";
+        elem.style.opacity = "0.1";
+    }
+    else{
+         let opacity = parseFloat(elem.style.opacity)+0.1;
+         if(opacity>1)opacity = 1;
+         elem.style.opacity = opacity.toString();
+    }
+}
 
+function enterGridSize(){
+    let gridSize = parseInt(prompt("Please enter number for grid size from 1 to 100"));
+    while(Number.isNaN(gridSize) || gridSize<1 || gridSize>100){
+        gridSize = parseInt(prompt("Invalid entry. Please enter number for grid size from 1 to 100"));
+    }
+    resizeGrid(gridSize);
+}
+
+function resizeGrid(gridSize){
+    const currGrid = document.querySelector("#grid-body");
+    currGrid.remove();
+    const newGrid = document.createElement("div");
+    newGrid.id = "grid-body";
+    for(let i = 0; i<gridSize; i++){
+        const row = document.createElement("div");
+        row.className = "grid-row";
+        for(let j = 0; j<gridSize; j++){
+            const box = document.createElement("div");
+            box.addEventListener("mouseover", (e) =>{
+                
+                setColor(e.target);
+                
+            })
+            box.className = "grid-box";
+            row.appendChild(box);
+        }
+        newGrid.appendChild(row);
+    }
+    document.body.appendChild(newGrid);
+}
+
+let sizeBtn = document.createElement("button");
+sizeBtn.innerText = "Resize Grid"
+sizeBtn.addEventListener("click", () =>{
+    enterGridSize();
+});
+
+
+
+document.body.appendChild(sizeBtn);
 document.body.appendChild(grid);
